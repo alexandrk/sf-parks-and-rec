bab5440aa5d2745a75b09c968af76abbbf23c5c9
Alexander Kazakov <alexander@kazakov.mobi>
2015-04-01 11:36:21 -0700

    Added pins, cleaned up the code

--- js/app.js
+++ js/app.js
@@ -1,83 +1,199 @@
-$(function(){
+var App = {};
 
-  var APP = function() {
+$(function(global) {
 
-    this.init = function() {
-      var mapCanvas = document.getElementById('map-canvas');
-      var mapOptions = {
-        center: new google.maps.LatLng(37.768920, -122.484255)
-        , zoom: 15
-        , mapTypeId: google.maps.MapTypeId.MAP      //TERRAIN
-        , disableDefaultUI: true
-        , zoomControlOptions: {
-            position: google.maps.ControlPosition.BOTTOM_CENTER
-            ,style: google.maps.ZoomControlStyle.SMALL
-          }
-      };
-
-      // Setting map-canvas width and height elements according to screen dimensions
-      //mapCanvas.css('width', window.screenX);
-      //mapCanvas.css('height', window.screenY);
-
-      this.map = new google.maps.Map(mapCanvas, mapOptions);
-
+  // Model
+  var Park = function(obj)
+  {
+    this.parktype   = obj.parktype;
+    this.parkname   = obj.parkname;
+    this.parkid     = obj.parkid;
+    this.location   = {
+      long:    obj.location_1.longitude,
+      lat:     obj.location_1.latitude,
+      address: obj.location_1.human_address.address,
+      city:    obj.location_1.human_address.city,
+      state:   obj.location_1.human_address.state,
+      zip:     obj.zipcode
     };
-
-    this.setMapStyles = function() {
-
-      this.map.set('styles', [
-        {
-          featureType: 'road',
-          elementType: 'geometry',
-          stylers: [
-            //{ color: '#cccccc' },
-            { weight: 1.5 }
-          ]
-        }
-        //, {
-        //  featureType: 'road',
-        //  elementType: 'labels',
-        //  stylers: [
-        //    { saturation: -100 },
-        //    { invert_lightness: true }
-        //  ]
-        //}, {
-        //  featureType: 'landscape',
-        //  elementType: 'geometry',
-        //  stylers: [
-        //    { hue: '#ffff00' },
-        //    { gamma: 1.4 },
-        //    { saturation: 82 },
-        //    { lightness: 96 }
-        //  ]
-        //}, {
-        //  featureType: 'poi.school',
-        //  elementType: 'geometry',
-        //  stylers: [
-        //    { hue: '#fff700' },
-        //    { lightness: -15 },
-        //    { saturation: 99 }
-        //  ]
-        //}
-      ]);
-    };
-
-  return {
-    init: this.init(),
-    setMapStyles: this.setMapStyles
+    this.acreage    = obj.acreage;
   };
 
-  }();
+  /**
+   * Centers Map on the location and adjust the zoom level
+   */
+  Park.prototype.centerMap = function(){
+    App.map.setCenter(new google.maps.LatLng(this.location.lat, this.location.long));
+    App.map.setZoom(17);
+  };
 
-  init();
-  setMapStyles();
+  /**
+   * getParksList
+   * Gets parks data from sfgov.org or local copy, if present and recent
+   */
+  function getParksList(){
+    App.parksCollection = [];
 
+    $.ajax({
+      dataType: 'json',
+      //async: false,
+
+      // URL for parks data from sfgov API
+      url: 'https://data.sfgov.org/resource/z76i-7s65.json',
+
+      success: function(data){
+
+        var geocoder = new google.maps.Geocoder(),
+            count = 0,
+            bounds = new google.maps.LatLngBounds();
+
+        data.forEach(function(item)
+        {
+          var park,
+              content,
+              infowindow;
+
+          if (typeof item.location_1 === 'object'){
+
+            // Exclude location outside of SF
+            if (
+              item.parkname.toUpperCase() == 'CAMP MATHER' ||
+              item.parkname.toUpperCase() == 'SHARP PARK'
+            ){
+              return;
+            }
+
+            // Parsing address property of the object
+            if (item.location_1){
+              item.location_1.human_address = JSON.parse(item.location_1.human_address);
+            }
+            else {
+              /**
+               * FIX - use delay to get GEO data from google api and save it
+               * to the address bit of the park data
+                */
+              //geocoder.geocode(
+              //  { 'address': item.parkname + ', San Francisco, CA' },
+              //  function(results, status){
+              //    console.log(' status: ' + status);
+              //    console.log('results: ' + results);
+              //  }
+              //);
+              item.location_1 = {
+                longtitude: "",
+                  latitude: "",
+                human_address: {
+                  address: "",
+                     city: "",
+                    state: ""
+                }
+              }
+            }
+
+            // Creating new Park object
+            park = new Park(item);
+
+            // Creating marker for each Park data object
+            park.mapMarker = new google.maps.Marker({
+              position: new google.maps.LatLng(park.location.lat, park.location.long),
+              map: App.map,
+              title: park.parkname
+            });
+
+            content = "<h3>" + park.parkname + "</h3><br />"
+            +"<div>Type: " + park.parktype + "</div>"
+            +"<div>Size: " + park.acreage + " acres</div>"
+            +"<div>Zip Code: " + park.location.zip + "</div>";
+
+            // Creating an info window for each marker object,
+            // with content coming from the template
+            infowindow = new google.maps.InfoWindow({
+              content: content,
+              maxWidth: 200
+            });
+
+            // Add park coords to bounds object (used to zoom out the map to fit all markers)
+            bounds.extend(park.mapMarker.position);
+
+            // Adding a marker click event with content for the info window
+            google.maps.event.addListener(park.mapMarker, 'mouseup', function(){
+              infowindow.open(App.map, park.mapMarker);
+            });
+
+            // Adding park data object to collection
+            App.parksCollection.push(park);
+          }
+
+          App.map.fitBounds(bounds);
+
+        });
+      },
+
+      error: function (xhr, textStatus, errorThrown) {
+        console.log('Failed to get parks data!');
+        console.log(errorThrown);
+      },
+
+      complete: function(){
+        $('body').trigger('parksData:loaded');
+      }
+
+    });
+  }
+
+  // Do steps below only after the data for the parks is done loading
+  $('body').on('parksData:loaded', function(){
+
+    //
+    ko.applyBindings({parks: App.parksCollection});
+
+    // List results pagination
+    $("div.resultsNav").jPages({
+      containerID : "results",
+      perPage: 10,
+      previous: "← prev"
+    });
+
+  });
+
+  /**
+   * Map Related
+   --------------------------------------------------------------------------------
+   */
+
+  /**
+   * mapInit
+   * @param container {string} - id of a map container
+   * @param mapOptionsDefault {object} - map options, if empty -> uses default options
+   */
+  function mapInit(container, mapOptionsDefault) {
+
+    mapOptions = mapOptionsDefault || {
+      center: new google.maps.LatLng(37.768920, -122.484255)
+      , zoom: 15
+      , mapTypeId: google.maps.MapTypeId.MAP      //TERRAIN
+      //, disableDefaultUI: true
+      , zoomControlOptions: {
+        //position: google.maps.ControlPosition.BOTTOM_LEFT,
+        style: google.maps.ZoomControlStyle.SMALL
+      }
+    };
+
+    return new google.maps.Map(document.getElementById(container), mapOptions);
+  }
+
+  /**
+   * UI Event Handlers
+   --------------------------------------------------------------------------------
+   */
+
+  // UI event handlers
   $('#up-arrow').on('click', function()
   {
     $('#up-arrow').hide();
     $('#down-arrow').show();
     $('#resultsWrapper').animate({
-      'top': '25%'
+      'top': '20%'
     });
   });
 
@@ -90,10 +206,15 @@ $(function(){
     });
   });
 
-  $("div.resultsNav").jPages({
-    containerID : "results",
-    perPage: 10,
-    previous: "← prev"
-  });
+  /**
+   * Execution Flow
+   --------------------------------------------------------------------------------
+   */
+
+  // Initialize Google Map
+  App.map = mapInit('map-canvas');
+
+  // Get Parks Data from sfgov.org website or use local copy, if present
+  getParksList();
 
 });
